3d8f5a517f82102772f31728fc19f29725e0b430
rkinnett <rkinnett@gmail.com>
2022-05-03 18:05:50 -0700

    bug fixes and general improvements

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Detect and control Kasa smart plugs by name or IP address from python or windows
     kasa_smart_plug_control 192.xxx.xxx.xxx off  
     kasa_smart_plug_control MyPlug1 on  
   
-  Compiled with pyinstaller:  
-    pyinstaller --noconfirm --onefile --console --icon "kasa_plug_icon.ico"  "kasa_smart_plug_control.py"  
+  Compiled with pyinstaller:
+	py -3 -m PyInstaller --noconfirm --onefile --console --name "kasa_smart_plug_control" --icon "kasa_plug_icon.ico" "kasa_smart_plug_control.py" --clean
   
   
 ### Or run from python:  
--- kasa_smart_plug_control.py
+++ kasa_smart_plug_control.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 import sys
 import re
-from kasa import SmartPlug, Discover
+from kasa import SmartPlug, Discover, SmartDeviceException
 '''
 KASA Smart Plug Controller
 
@@ -12,13 +12,22 @@ https://github.com/rkinnett/kasa_smart_plug_control
 Requires python-kasa:
   pip install python-kasa
 
-Syntax:
-  python kasa_smart_plug_control discover
-  python kasa_smart_plug_control 192.xxx.xxx.xxx info
-  python kasa_smart_plug_control MySmartPlug1 on
-  python kasa_smart_plug_control MySmartPlug1 off
-
 KASA API:  https://python-kasa.readthedocs.io/en/latest/index.html
+
+
+'''
+
+help_info = '''
+Kasa Smart Plug Controller
+
+(c) Ryan Kinnett, 2022
+https://github.com/rkinnett/kasa_smart_plug_control
+
+Syntax:
+  kasa_smart_plug_control discover
+  kasa_smart_plug_control 192.xxx.xxx.xxx info
+  kasa_smart_plug_control MySmartPlug1 on
+  kasa_smart_plug_control MySmartPlug1 off
 '''
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
@@ -30,8 +39,17 @@ async def main():
     operation = None
     plug = None
     
+    if len(sys.argv)<=1:
+        print(help_info)
+        time.sleep(5)
+        return
+        
     for arg in sys.argv[1:]:
-        if arg.lower() in ('discover', 'on', 'off', 'info'):
+        if re.match(r'^-*[hH](elp)?$', arg) or arg=='/?':
+            print(help_info)
+            time.sleep(5)
+            return
+        elif arg.lower() in ('discover', 'list', 'on', 'off', 'info'):
             print('Requested operation: %s' % arg)
             operation = arg
         elif re.match('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', arg):
@@ -41,35 +59,43 @@ async def main():
             target_name = arg
             devices = await discover()
 
-    if operation in (None, 'discover'):
+    if operation in (None, 'discover', 'list'):
         devices = await discover()
         return
 
     if target_ip:
         plug = SmartPlug(target_ip)
     elif target_name:
-        assert devices is not None and len(devices.items()) > 0, 'Error, no kasa smar plugs detected'
+        if not devices or len(devices.items()) == 0:
+            print('Error, no kasa smar plugs detected')
+            return
         for addr, device in devices.items():
             if device.alias.lower() == target_name.lower():
                 print('found match')
                 plug = device
                 break
-        assert plug is not None, 'failed to identify plug "%s"' % target_name
+        if plug is None:
+            print('failed to identify plug "%s"' % target_name)
+            return
         
-    await plug.update()
+    try:
+        await plug.update()
+    except SmartDeviceException as e:
+        print(e)
+        return
     
     if operation == 'on':
-        if device.is_on:
-            print('Plug "%s" is already on' % device.alias)
+        if plug.is_on:
+            print('Plug "%s" is already on' % plug.alias)
         else:
-            print('Turning on plug "%s"' % device.alias)
+            print('Turning on plug "%s"' % plug.alias)
             await plug.turn_on()
     elif operation == 'off':
-        if device.is_on:
-            print('Turning off plug "%s"' % device.alias)
+        if plug.is_on:
+            print('Turning off plug "%s"' % plug.alias)
             await plug.turn_off()
         else:
-            print('Plug "%s" is already off' % device.alias)
+            print('Plug "%s" is already off' % plug.alias)
     elif operation == 'info':
         print(plug)
 
@@ -80,8 +106,7 @@ async def discover():
     devices = await Discover.discover()
     for addr, dev in devices.items():
         await dev.update()
-        print(f"{addr} >> {dev}")
-    print('(done)')
+        print('  "%s", plug type %s at %s' % (dev.alias, dev.model, addr))        
     return devices
 
 
